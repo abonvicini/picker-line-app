87f7401b9406668a7307eddf6f2e5d74d50b713e
Alexis Bonvicini <alexisbonvicini@gmail.com>
2021-09-06 00:33:09 -0300

    Component created: Form

---
+++ src/components/Form.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { Divider, TextField, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(2,),
+      width: '95%',
+    },
+  }
+}));
+
+
+
+function Form({ name, numbers, onSubmit }) {
+
+  const [formName, setFormName] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(formName)
+  }
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setFormName(event.target.value)
+  }
+
+  const classes = useStyles();
+  return (
+    <div>
+      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <h3>Detalle:</h3>
+        <p>
+          Usted a seleccionado la {name} que contiene los siguientes numeros {numbers}
+        </p>
+        <Divider />
+        <TextField id="standard-basic" label="Ingrese su nombre" value={formName} onChange={handleChange} />
+        <Button size="large" variant="contained" color="primary" type='submit'  >
+          Confirmar
+        </Button>
+
+      </form>
+
+
+    </div>
+  )
+}
+
+export default Form
